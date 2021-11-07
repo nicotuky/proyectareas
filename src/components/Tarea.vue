4cc986a44f8a642b9f50e1407a2658570e1e3de8
DamiiPeralta <86084164+DamiiPeralta@users.noreply.github.com>
2021-11-07 01:50:09 -0300

    Update Final Local

--- src/components/Tarea.vue
+++ src/components/Tarea.vue
@@ -14,17 +14,27 @@
                             </div>         
                         </div>
                         <br>
+                        <div class="text-center">
+                        <div v-if="loading" class="spinner-border text-success" role="status">
+                          <span class="sr-only">Loading...</span>
+                          </div>
+                          </div>
                         <h5 v-if="listarea.length == 0">No hay laburo</h5>
+                          <ul v-if="!loading" class="list-group">
 
-                        <ul class="list-group">
-                          <li v-for="(tarea,index) of listarea" :key="index" 
-                          class="list-group-item d-flex justify-content-between">
-                            <span class="cursor" v-bind:class="{'text-success' : tarea.estado}"
-                            v-on:click="editartarea(tarea, index)">
+                            <li
+                              v-for="(tarea, index) of listarea" 
+                              :key="index" 
+                              class="list-group-item d-flex justify-content-between">
+                            
+                            <span 
+                              class="cursor" 
+                              v-bind:class="{'text-success' : tarea.estado}"
+                              v-on:click="editartarea(tarea, tarea.id)">
                               <i v-bind:class="[ tarea.estado ? 'fas fa-clock' : 'far fa-circle']"></i>
                             </span>
                             {{tarea.nombre}}
-                            <span class="text-danger cursor" v-on:click="elimitarTarea(index)">
+                            <span class="text-danger cursor" v-on:click="eliminarTarea(tarea.id)">
                               <i class="fas fa-bars"></i>
                               </span>
                           </li>
@@ -37,12 +47,14 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'tarea',
   data(){
     return {
       tarea: '',
-      listarea: []
+      listarea: [],
+      loading: false
     }
   },
   methods: {
@@ -51,15 +63,50 @@ export default {
         nombre: this.tarea,
         estado: false
       }
-      this.listarea.push(tarea);
+      /*this.listarea.push(tarea);*/
+      this.loading = true;
+      axios.post("https://localhost:44349/api/Tarea/", tarea)
+      .then(response => {
+        console.log(response);
+        this.loading = false;
+        this.obtenerTareas();
+      }).catch(error => {
+        console.error(error);
+        this.loading = false;
+      })
       this.tarea ='';
     },
-    elimitarTarea(index){
-      this.listarea.splice(index,1)
+    eliminarTarea(id){
+      /*this.listarea.splice(index,1)*/
+      axios.delete("https://localhost:44349/api/Tarea/" + id).then(response => {
+        console.log(response);
+        this.obtenerTareas();
+        this.loading = false;
+      }).catch(error => {
+        console.log(error);
+        this.loading = false;
+      } )
     },
-    editartarea(tarea,index){
-      this.listarea[index].estado = !tarea.estado
+    editartarea(tarea,id){
+      /*this.listarea[index].estado = !tarea.estado */
+      this.loading = true;
+      axios.put("https://localhost:44349/api/Tarea/" + id, tarea).then(()=>{
+        this.obtenerTareas();
+        this.loading = false
+      }).catch(()  => this.loading = false)
+    },
+    obtenerTareas(){
+      this.loading = true;
+      axios.get("https://localhost:44349/api/Tarea/")
+      .then(response => {
+        console.log(response);
+        this.listarea = response.data;
+        this.loading = false;
+      }).catch(() => this.loading = false)
     }
+  },
+  created: function() {
+    this.obtenerTareas();
   }
 }
 </script>
